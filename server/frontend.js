64cb126dfa883ba7751bb0d495440a727ec6f0fc
Alejandro Esquivel <dralejandro604@gmail.com>
2019-04-01 14:00:01 -0700

    Added app-root-path

--- server/frontend.js
+++ server/frontend.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const appRoot = require('app-root-path');
 
 const app = express();
 
 const PORT = 8080;
 
-app.use(express.static('../dist'));
+const dir = `${appRoot}/dist`;
+
+app.use(express.static(dir));
 
 app.get('/',(req,res)=>res.status(200).send('ok'));
 
-app.listen(PORT,()=>console.log(`Listening on port ${PORT}...`));
+app.listen(PORT,()=>console.log(`Listening on port ${PORT}, serving ${dir}...`));
